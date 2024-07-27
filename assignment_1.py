3f9fbaf9a3bc622a2a3f37b5b440feca4048258d
hafizamir <hafizamir588@gmail.com>
2024-07-27 11:10:04 +0500

    calculate ages

---
+++ assignment_1.py
@@ -0,0 +1,22 @@
+# Problem 1:
+
+def calcculate_age():
+    anton_age = 21
+    beth_age = anton_age + 6
+    chen_age = beth_age + 20
+    drew_age = chen_age + anton_age
+    ethan_age = chen_age
+
+    return {
+        'Anton': anton_age,
+        'Beth': beth_age,
+        'Chen': chen_age,
+        'Drew': drew_age,
+        'Ethan': ethan_age
+    }
+
+# Print the results
+ages = calcculate_age()
+for f, age in ages.items():
+    print(f"{f} is {age}")
+
